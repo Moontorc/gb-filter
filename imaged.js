38aaf51f6e8f2a624be1ae6a0ffee5d67e55bb4d
Moontorc <adam.e.mcmanus@gmail.com>
2022-02-14 15:08:57 +0000

    Create imaged.js

---
+++ imaged.js
@@ -0,0 +1,36 @@
+﻿// ------------------------------------------------------------------------------------------------
+// シェーダー（テクスチャマッピング）
+// ------------------------------------------------------------------------------------------------
+
+
+// -------------------------------------------------------------------------------------------
+// 頂点シェーダー
+var vs_imaged = `
+attribute vec3 a_position;
+attribute vec2 a_uv0;
+uniform mat4 u_wvp;
+varying vec2 v_uv0;
+
+void main()
+{
+	v_uv0 = a_uv0;
+	gl_Position = u_wvp * vec4(a_position, 1.0);
+}
+`;
+
+
+// -------------------------------------------------------------------------------------------
+// フラグメントシェーダー
+var fs_imaged = `
+/*precision mediump float;*/
+precision highp float;
+
+uniform sampler2D u_texture0;
+varying vec2 v_uv0;
+
+void main()
+{
+	gl_FragColor = texture2D(u_texture0, v_uv0);
+}
+`;
+
